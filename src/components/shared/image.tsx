4895a4bd1e63c4b95ec78c58745236b6dd13787b
Luke Ramljak <lukeramljak@proton.me>
2025-02-02 15:14:28 +0800

    Download individual images

--- src/components/shared/image.tsx
+++ src/components/shared/image.tsx
@@ -1,4 +1,4 @@
-import { ResizedImage } from "@/lib/img-utils";
+import { downloadImage, ResizedImage } from "@/lib/img-utils";
 import Image from "next/image";
 
 interface ImageRendererProps {
@@ -7,18 +7,21 @@ interface ImageRendererProps {
 
 const ImageCard = ({ image }: ImageRendererProps) => {
   return (
-    <div className="flex flex-col justify-between border border-white/30 rounded-md">
+    <div
+      className="flex flex-col justify-between border border-white/30 rounded-md cursor-pointer transition-colors hover:bg-white/10"
+      onClick={() => downloadImage(image)}
+    >
       <div className="flex items-center justify-center h-[calc(112px+2rem)]">
         <Image
           src={image.content}
-          alt={`Preview of ${image.size}x${image.size} image`}
-          width={image.size}
-          height={image.size}
+          alt={`Preview of ${image.metadata.width}x${image.metadata.height} image`}
+          width={image.metadata.width}
+          height={image.metadata.height}
         />
       </div>
       <div className="flex flex-col items-center justify-between p-2 text-xs text-gray-200 bg-white/10 sm:flex-row">
         <span>
-          {image.size}px x {image.size}px
+          {image.metadata.width}px x {image.metadata.height}px
         </span>
         <span className="font-bold">{image.fileSize}KB</span>
       </div>
--- src/lib/img-utils.ts
+++ src/lib/img-utils.ts
@@ -1,7 +1,11 @@
 export interface ResizedImage {
-  size: number;
   content: string;
   fileSize: string;
+  metadata: {
+    width: number;
+    height: number;
+    name: string;
+  };
 }
 
 export const convertImageToMultipleSizes = async (
@@ -49,9 +53,13 @@ export const convertImageToMultipleSizes = async (
       );
 
       return {
-        size,
         content: resizedCanvas.toDataURL("image/png"),
         fileSize: bytesToKilobytes(estimateCanvasFileSize(resizedCanvas)),
+        metadata: {
+          width: size,
+          height: size,
+          name: imageMetadata.name,
+        },
       };
     }),
   );
@@ -74,4 +82,11 @@ export const estimateCanvasFileSize = (canvas: HTMLCanvasElement): number => {
 
 const bytesToKilobytes = (bytes: number): string => {
   return (bytes / 1024).toFixed(2);
+};
+
+export const downloadImage = (image: ResizedImage): void => {
+  const link = document.createElement("a");
+  link.href = image.content;
+  link.download = `${image.metadata.name.replace(/\..+$/, "")}@${image.metadata.width}.png`;
+  link.click();
 };
